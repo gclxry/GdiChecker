73653f0e8216d39ac67f881c84b9f3978807a00d
plxaye <307685235@qq.com>
2014-09-11 17:37:25 +0800

    add empty menu
    增加一个空菜单

--- GdiChecker/resource.h
+++ GdiChecker/resource.h
@@ -1,21 +1,20 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by GdiChecker.RC
+// Used by GdiChecker.rc
 //
-
-
-#define IDD_ABOUTBOX				100
-#define IDR_MAINFRAME				128
-//#define IDR_GDICHECKERTYPE	129
-#define IDD_MAINDLG				129
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDD_MAINDLG                     129
+#define IDR_MENU1                       201
+#define IDR_MENU_MAIN                   201
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE	201
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		101
-#define _APS_NEXT_COMMAND_VALUE		32775
+#define _APS_NEXT_RESOURCE_VALUE        202
+#define _APS_NEXT_COMMAND_VALUE         32775
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
